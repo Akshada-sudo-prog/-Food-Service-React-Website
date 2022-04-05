d0292ba556fac48d5dba11668ca0a6ae514d7d3a
Akshada-sudo-prog <84959326+Akshada-sudo-prog@users.noreply.github.com>
2022-04-05 20:16:42 +0530

    my first commit reactwebsite

---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,87 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/apple.jpg",
+      name: "Apple",
+      category: "GLOW FOOD",
+      price: "30₹",
+      description:
+        "Apples are an incredibly nutritious fruit that offers multiple health benefits. .",
+    },
+  
+    {
+      id: 2,
+      image: "images/chickeen.jpg",
+      name: "Chicken",
+      category:"GROW FOOD" ,
+      price: "70₹",
+      description:
+        "Chicken is a lean meat with high nutritional.it regularly will help you stay healthy. ",
+    },
+    {
+      id: 3,
+      image: "images/bread.jpg",
+      name: "Bread",
+      category: "GO FOOD",
+      price: "10₹",
+      description:
+        "In moderation, bread can be enjoyed as part of a healthy diet.help you stay healthy. ",
+    },
+    {
+      id: 4,
+      image: "images/Carrot.jpg",
+      name: "Carrot",
+      category: "GLOW FOOD",
+      price: "50₹",
+      description:
+        "Carrots have calcium and vitamin K, both of which are important for bone health. ",
+    },
+    {
+      id: 5,
+      image: "images/egg and milk.jpg",
+      name: "Egg and Milk",
+      category: "GROW FOOD",
+      price: "80₹",
+      description:
+        "Eggs and milk together are extremely rich in protein, sodium, and calcium. ",
+    },
+    {
+      id: 6,
+      image: "images/rice.jpg",
+      name: "Rice",
+      category: "GO FOOD",
+      price: "180₹",
+      description:
+        "rice contains vitamins and minerals that help the blood transport oxygen .",
+    },
+    {
+      id: 7,
+      image: "images/brocccoli.jpg",
+      name: "broccoli",
+      category: "GLOW FOOD",
+      price: "60₹",
+      description:
+        "Broccoli is a nutrient-rich vegetable that may enhance your health.May be good for eye health.",
+    },
+    {
+      id: 8,
+      image: "images/fish.jpg",
+      name: "Fish",
+      category: "GROW FOOD",
+      price: "60₹",
+      description:
+        "The American Heart Association recommends eating fish at least two times per week as part of a healthy diet. ",
+    },
+    {
+      id: 9,
+      image: "images/pasta.jpg",
+      name: "Pasta",
+      category: "GO FOOD",
+      price: "10₹",
+      description:
+        "It's a good source of energy and can give you fiber, too, if it's made from whole grain",
+    },
+  ];
+  
+  export default Menu;
+  
